96605668178b0a73e3364283fe8e8f6651f8c8f1
Manan Maniyar <maniyarmanan1996@gmail.com>
2018-07-02 03:56:10 -0700

    added show posts code

--- Pearing_Up/SavedPostsViewController.swift
+++ Pearing_Up/SavedPostsViewController.swift
@@ -2,7 +2,7 @@
 //  SavedPostsViewController.swift
 //  Pearing_Up
 //
-//  Created by Navin Kumar Ravindra on 2018-06-30.
+//  Created by Manan Maniyar on 2018-06-30.
 //  Copyright © 2018 Manan Maniyar. All rights reserved.
 //
 
@@ -10,69 +10,109 @@ import UIKit
 import Alamofire
 import SwiftyJSON
 
-class SavedPostsViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
-    
-    // Add the Logic the get all the saved posts from the server
-    
-    // https://pearingup.herokuapp.com/:sender_username/saved_posts
+class SavedPostsViewController: UIViewController, UICollectionViewDataSource{
     
     let bookmarks_url : URL = URL(string: "https://pearingup.herokuapp.com/manan/savedposts")!
-   
+    let posts_url : URL = URL(string: "https://pearingup.herokuapp.com/getpost/apple_post")!
+    var posts_city : [String] = []
     let tArray = ["A", "B", "C", "D", "E"]
+    var posts_name : [String]  = []
+    let temp_posts = ["apple_post", "banana_post"]
     let tImage = [UIImage(named: "tree"), UIImage(named: "tree1"), UIImage(named: "tree2"), UIImage(named: "tree2"), UIImage(named: "tree")]
     let tDescription = ["A", "B", "C", "D", "E"]
     let tFruits = ["Apples", "Bananas", "Kiwi", "Oranges", "Pineapple"]
     let tCity = ["Vancouver", "Burnaby", "Surrey", "Coquitlam", "Richmond"]
     
+    @IBOutlet weak var bookmarked_posts: UICollectionView!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        get_bookmarks(url: bookmarks_url)
-        self.collectionView(<#T##collectionView: UICollectionView##UICollectionView#>, cellForItemAt: <#T##IndexPath#>)
+        bookmarked_posts.dataSource = self
+        get_bookmarks(url: bookmarks_url) { response in
+            let all_posts = response["result"].array
+            for posts in all_posts! {
+                //print(posts)
+                self.posts_name.append(String(describing: posts))
+            }
+            print(self.posts_name.count)
+        }
+        update_data() { data in
+            print(data)
+        }
+        
+    }
+    func update_data(completion : (String) -> Void) {
+        print("Here")
+        for post in self.posts_name {
+            let PostURl : URL = URL(string: "https://pearingup.herokuapp.com/getpost/\(post)")!
+            print("Post = ", post)
+            self.get_allPosts(url: PostURl) { resp in
+                print("Received Response")
+                let city = resp["title"].string
+                self.posts_city.append(city!)
+            }
+        }
+        completion("Done")
+    }
+
+    
+    func get_allPosts(url: URL, completion : @escaping (JSON) -> Void) {
+        Alamofire.request(url, method: .get).responseJSON {
+            response in
+            if(response.result.isSuccess) {
+                let temp : JSON = JSON(response.result.value!)
+                print("Success in get-posts route")
+                completion(temp)
+            }
+            else {
+                print(response.result.error!)
+                completion("error")
+            }
+        }
     }
     
+ 
     
-    // Here passing all the Alamofire requests
-    
-    func get_bookmarks(url: URL) {
+    func get_bookmarks(url: URL, completion : @escaping (JSON) -> Void) {
         Alamofire.request(url, method: .get).responseJSON {
             response in
             if(response.result.isSuccess) {
                 let temp : JSON = JSON(response.result.value!)
                 print("Success in the get-saved-posts route")
-                //print(response.result.value!)
-                if(temp["code"] == 302 || temp["code"] == 400 || temp["code"] == 409){
-                    self.displayAlert(message: String(describing: temp["result"]))
-                    return
-                }
+                //print(response.result!)
+                completion(temp)
             }
             else {
                 print("Error happened")
+                completion(response.result.error! as! JSON)
             }
         }
     }
+    
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return tArray.count
     }
     
     
-     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let saved_posts = collectionView.dequeueReusableCell(withReuseIdentifier: "saved_posts_cell", for: indexPath) as! CollectionViewCell
-        
         saved_posts.postImage.image = tImage[indexPath.row]
-        saved_posts.postCity.text = tCity[indexPath.row]
-        saved_posts.postFruit.text = tFruits[indexPath.row]
-        saved_posts.postDescription.text = tDescription[indexPath.row]
-        saved_posts.postTitle.text = tArray[indexPath.row]
-        
-        
+        saved_posts.postCity.text! = tCity[indexPath.item]
+        saved_posts.postFruit.text! = tFruits[indexPath.item]
+        saved_posts.postDescription.text! = tDescription[indexPath.item]
+        saved_posts.postTitle.text! = tArray[indexPath.item]
         return saved_posts
     }
     
+//    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+//        print(indexPath.row)
+//    }
+    
+    
+    
     func displayAlert(message: String){
-        print("Function being called")
         let alert_toDisplay = UIAlertController(title: "Alert", message: message, preferredStyle: .alert)
         alert_toDisplay.addAction(UIAlertAction(title: "Ok", style: .default, handler: { action in
             print("Error")
@@ -81,16 +121,3 @@ class SavedPostsViewController: UIViewController, UICollectionViewDataSource, UI
     }
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
