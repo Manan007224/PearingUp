67d2159b5b69cb567e76131dc4bce2c7c1bfb1c2
a-arshad <ali.stick.arshad@gmail.com>
2018-07-04 02:03:22 -0700

    Expanded Request View hooked up

--- Pearing_Up/InboxViewController.swift
+++ Pearing_Up/InboxViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
 class InboxViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
@@ -14,13 +16,13 @@ class InboxViewController: UIViewController, UITableViewDelegate, UITableViewDat
     let nameList = ["User1", "User2", "User3", "User4", "User5"]
     
     //populate this array with attached messages
-    let descriptionList = ["Hello", "I am a test" , "Hey" ,"Let me come over" , "I want to pick your apples"]
+    let descriptionList = ["Hello", "I am a test" , "Hey" ,"Let me come over" , "I want to pick your apples. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."]
     
     public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
     {
         return(nameList.count)
     }
-
+	
     
     public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
     {
@@ -43,6 +45,22 @@ class InboxViewController: UIViewController, UITableViewDelegate, UITableViewDat
         // Dispose of any resources that can be recreated.
     }
     
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+        self.performSegue(withIdentifier: "expandRequest", sender: indexPath)
+        
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "expandRequest" {
+            if let collectionCell: InboxTableViewCell = sender as? InboxTableViewCell {
+                if let destination = segue.destination as? ExpandedRequestViewController {
+                    destination.requestName = collectionCell.myLabel.text!
+                    destination.requestDespcription = collectionCell.descriptionLabel.text!
+                }
+            }
+        }
+    }
 
     /*
     // MARK: - Navigation
