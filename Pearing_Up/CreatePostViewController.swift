09c22eebe7f4f42a4d09bfa8211cbb7964c42a0b
yoshiumw <yoshiumw@asb9700-e01.csil.sfu.ca>
2018-07-10 19:44:29 -0700

    Colour is changed for tab bar

--- Pearing_Up/CreatePostViewController.swift
+++ Pearing_Up/CreatePostViewController.swift
@@ -127,38 +127,6 @@ class MakePostViewController: UIViewController {
         self.present(actionSheet, animated:true, completion: nil)
     }
     
-
-    
-//    @IBAction func addImg(_ sender: Any) {
-//        print("yollllll")
-//        let imagePickerController = UIImagePickerController()
-//        imagePickerController.delegate = self
-//
-//        let actionSheet = UIAlertController(title: "Photo Source", message: "Choose a Source", preferredStyle: .actionSheet)
-//        actionSheet.addAction(UIAlertAction(title: "Camera", style: .default, handler: {(action:UIAlertAction) in
-//
-//            if UIImagePickerController.isSourceTypeAvailable(.camera){
-//                imagePickerController.sourceType = .camera
-//                self.present(imagePickerController, animated: true, completion: nil)
-//            } else {
-//                print("camera not available")
-//            }
-//
-//            imagePickerController.sourceType = .camera
-//            self.present(imagePickerController, animated: true, completion: nil)
-//        }))
-//
-//        actionSheet.addAction(UIAlertAction(title: "Photo Library" , style: .default, handler: {(action:UIAlertAction) in imagePickerController.sourceType = .photoLibrary
-//            self.present(imagePickerController, animated: true, completion: nil)
-//        }))
-//
-//        actionSheet.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil ))
-//
-//        self.present(actionSheet, animated:true, completion: nil)
-//
-//    }
-//
-    
     
     func imagePickerController( _ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String: Any]) {
         let image = info[UIImagePickerControllerOriginalImage] as! UIImage
