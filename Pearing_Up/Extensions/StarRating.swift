e45c4afc73e98d180d544bddd954bddacfbf50be
Grace Kim <gracekim@d207-023-205-086.wireless.sfu.ca>
2018-07-31 13:28:02 -0700

    rating

---
+++ Pearing_Up/Extensions/StarRating.swift
@@ -0,0 +1,21 @@
+//
+//  StarRating.swift
+//  Pearing_Up
+//
+//  Created by Grace Kim on 2018-07-31.
+//  Copyright © 2018 Manan Maniyar. All rights reserved.
+//
+
+import UIKit
+
+class StarRating: UIStackView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
