e2e5dbb7183be507e37188415408fe46e6c1a31c
Grace Kim <gracekim@d207-023-205-086.wireless.sfu.ca>
2018-07-31 13:30:19 -0700

    rating

--- Pearing_Up/Extensions/StarRating.swift
+++ Pearing_Up/Extensions/StarRating.swift
@@ -2,20 +2,35 @@
 //  StarRating.swift
 //  Pearing_Up
 //
-//  Created by Grace Kim on 2018-07-31.
+//  Created by Grace Kim on 2018-07-21.
 //  Copyright © 2018 Manan Maniyar. All rights reserved.
 //
 
 import UIKit
 
 class StarRating: UIStackView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setUpButtons()
     }
-    */
+    
+    required init(coder: NSCoder) {
+        super.init(coder: coder)
+        setUpButtons()
+    }
+    
+    private func setUpButtons() {
+        
+        let button = UIButton()
+        button.backgroundColor = .green
+        button.translatesAutoresizingMaskingIntoConstraints = false
+        button.heightAnchor.constraint(equalToConstant: 44.0).isActive = true
+        button.widthAnchor.constraint(equalToConstant: 44.0).isActive = true
+        
+        addArrangedSubview(button)
+    }
+
 
 }
